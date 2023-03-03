cfd60e758450a518cb12be727aab01dbb8567126
Chaitanya9124 <kotturichaitanya2000@gmail.com>
2023-03-03 15:49:39 -0500

    basiscs added

--- src/basics.java
+++ src/basics.java
@@ -7,7 +7,7 @@ public class basics {
 //        while (count !=5){
 //            System.out.println(count);
 //            count++;
-
+// learn push
         // for loop
         for(int count = 1; count != 5; count++){
             System.out.println(count);
