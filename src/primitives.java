ca3e8ade8ac8ab9a614d46002b39dc0f0fc57a8b
Chaitanya9124 <kotturichaitanya2000@gmail.com>
2023-03-03 15:13:57 -0500

    intial commit

---
+++ src/primitives.java
@@ -0,0 +1,11 @@
+public class primitives {
+    public static void main(String[] args) {
+        int rollnumber = 64;
+        char letter = 'r';
+        float marks = 98.64f;
+        double largedecimalnumbers = 4567893.87654;
+        long largeinteger = 234567889L;
+        boolean check = false;
+
+    }
+}
